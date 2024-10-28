0abe250e8600e4dc18315170b3be11f84d12bccf
Ramya-droid <ramyavedhavathim@gmail.com>
2024-10-28 14:43:18 +0530

    data ingestion notebook added

---
+++ src/textsummarizer/entity/__init__.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=False)
+class DataIngestionConfig:
+    def __init__(self, root_dir, source_URL, local_data_file, unzip_dir):
+        self.root_dir = root_dir
+        self.source_URL = source_URL
+        self.local_data_file = local_data_file
+        self.unzip_dir = unzip_dir
